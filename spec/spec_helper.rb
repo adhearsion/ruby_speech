3262444daabbe09dd1782e7499c694d6ebcd80e7
Taylor Carpenter <taylor@codecafe.com>
2011-09-11 03:20:10 -0500

    grxml helper code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,12 @@ puts "Loading the SSML Schema from #{schema_file_path}..."
 SSML_SCHEMA = Nokogiri::XML::Schema File.open(schema_file_path)
 puts "Finished loading schema."
 
+schema_file_path = File.expand_path File.join(__FILE__, '../../assets/grammar.xsd')
+puts "Loading the GRXML Schema from #{schema_file_path}..."
+GRXML_SCHEMA = Nokogiri::XML::Schema File.open(schema_file_path)
+puts "Finished loading schema."
+
+
 RSpec.configure do |config|
   config.mock_with :mocha
   config.filter_run :focus => true

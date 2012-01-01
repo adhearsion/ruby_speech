82dc8b4f49c08be336713832d16be4cb5e02a57f
Ben Langfeld <ben@langfeld.me>
2012-01-01 11:09:29 +0000

    [BUGFIX] Un-mark a passing, pending spec

--- spec/ruby_speech/grxml/grammar_spec.rb
+++ spec/ruby_speech/grxml/grammar_spec.rb
@@ -46,7 +46,7 @@ module RubySpeech
 
         it { should be_instance_of Grammar }
 
-        its(:language)  { pending; should == 'jp' }
+        its(:language)  { should == 'jp' }
         its(:base_uri)  { should == 'blah' }
         its(:mode)      { should == :dtmf }
         its(:root)      { should == 'main_rule' }

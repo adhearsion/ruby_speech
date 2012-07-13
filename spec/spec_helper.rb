66bf3c6249be7378228b6deb301ea0cdb5754b57
Ben Langfeld <ben@langfeld.me>
2012-07-13 16:38:22 +0100

    [CS] Whitespace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@ puts "Loading the GRXML Schema from #{schema_file_path}..."
 GRXML_SCHEMA = Nokogiri::XML::Schema File.open(schema_file_path)
 puts "Finished loading schema."
 
-
 RSpec.configure do |config|
   config.mock_with :mocha
   config.filter_run :focus => true

c55dc99e8cf5d9772d51ac603de96d4683ba4635
Ben Langfeld <ben@langfeld.me>
2013-01-07 11:20:32 -0500

    [FEATURE] Allow parsing SSML, GRXML & NLSML documents through a single path

--- lib/ruby_speech.rb
+++ lib/ruby_speech.rb
@@ -21,7 +21,15 @@ module RubySpeech
 
   def self.parse(string)
     document = Nokogiri::XML.parse string, nil, nil, Nokogiri::XML::ParseOptions::NOBLANKS
-    NLSML::Document.new document
+    namespace = document.root.namespace
+    case namespace && namespace.href
+    when SSML::SSML_NAMESPACE
+      SSML::Element.import string
+    when GRXML::GRXML_NAMESPACE
+      GRXML::Element.import string
+    when NLSML::NLSML_NAMESPACE, nil
+      NLSML::Document.new document
+    end
   end
 end
 

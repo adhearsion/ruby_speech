e4eb70787a5458405167cf0bedc701b9632844da
Ben Langfeld <ben@langfeld.me>
2013-09-29 00:29:59 -0300

    [CS] Make builtin grammar specs fail nicer with interpretation mismatch

--- spec/support/grammar_matchers.rb
+++ spec/support/grammar_matchers.rb
@@ -8,8 +8,8 @@ end
 
 RSpec::Matchers.define :match do |input|
   match do |grammar|
-    result = RubySpeech::GRXML::Matcher.new(grammar).match(input)
-    result.is_a?(RubySpeech::GRXML::Match) && result.interpretation == @interpretation
+    @result = RubySpeech::GRXML::Matcher.new(grammar).match(input)
+    @result.is_a?(RubySpeech::GRXML::Match) && @result.interpretation == @interpretation
   end
 
   chain :and_interpret_as do |interpretation|
@@ -18,13 +18,26 @@ RSpec::Matchers.define :match do |input|
 
   description do
     %{#{default_description} and interpret as "#{@interpretation}"}
+  end
+
+  failure_message_for_should do |grammar|
+    messages = []
+    unless @result.is_a?(RubySpeech::GRXML::Match)
+      messages << "expected a match result, got a #{@result.class}"
+    end
+
+    unless @result.interpretation == @interpretation
+      messages << %{expected interpretation to be "#{@interpretation}" but received "#{@result.interpretation}"}
+    end
+
+    messages.join(' ')
   end
 end
 
 RSpec::Matchers.define :max_match do |input|
   match do |grammar|
-    result = RubySpeech::GRXML::Matcher.new(grammar).match(input)
-    result.is_a?(RubySpeech::GRXML::MaxMatch) && result.interpretation == @interpretation
+    @result = RubySpeech::GRXML::Matcher.new(grammar).match(input)
+    @result.is_a?(RubySpeech::GRXML::MaxMatch) && @result.interpretation == @interpretation
   end
 
   chain :and_interpret_as do |interpretation|
@@ -33,5 +46,18 @@ RSpec::Matchers.define :max_match do |input|
 
   description do
     %{#{default_description} and interpret as "#{@interpretation}"}
+  end
+
+  failure_message_for_should do |grammar|
+    messages = []
+    unless @result.is_a?(RubySpeech::GRXML::Match)
+      messages << "expected a match result, got a #{@result.class}"
+    end
+
+    unless @result.interpretation == @interpretation
+      messages << %{expected interpretation to be "#{@interpretation}" but received "#{@result.interpretation}"}
+    end
+
+    messages.join(' ')
   end
 end

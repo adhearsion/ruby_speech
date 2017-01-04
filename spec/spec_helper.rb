896946566bb2af1b9de33f748256206ee42f3084
Stephen George <sgit-turtle.testudo12@wronghead.com>
2017-01-04 15:22:13 -0600

    [SPEC] Fix code coverage reporting: ensure nokogiri is loaded first

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,9 @@
 
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 
+Coveralls.wear!
+require "ruby_speech"
+
 schema_file_path = File.expand_path File.join(__FILE__, '../../assets/synthesis.xsd')
 puts "Loading the SSML Schema from #{schema_file_path}..."
 SSML_SCHEMA = Nokogiri::XML::Schema File.open(schema_file_path)
@@ -23,6 +26,3 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
 end
-
-Coveralls.wear!
-require "ruby_speech"

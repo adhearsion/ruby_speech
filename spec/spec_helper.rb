510040e621f600fbcbafd8bbf13d052b164f07b3
Stephen George <sgit-turtle.testudo12@wronghead.com>
2017-01-04 16:17:55 -0600

    [SPEC] Fix code coverage reporting: Enable SimpleCov HTML formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require "coveralls"
-Coveralls.wear!
+%w{
+  simplecov
+  coveralls
+}.each { |f| require f }
+
+SimpleCov.formatters << SimpleCov::Formatter::HTMLFormatter
+SimpleCov.start
 
 %w{
   rspec/its

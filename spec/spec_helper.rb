b4012194f9af1b58e71d06c45243700abee30264
Stephen George <sgit-turtle.testudo12@wronghead.com>
2017-01-04 18:06:36 -0600

    [SPEC] Fix code coverage reporting: Re-enable Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,10 @@
   coveralls
 }.each { |f| require f }
 
-SimpleCov.formatters << SimpleCov::Formatter::HTMLFormatter
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start
 
 %w{
